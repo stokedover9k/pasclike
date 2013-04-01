90fa92667259cbc87f496f448874124759c990c9
stokedover9k <stokedover9k@gmail.com>
2013-03-31 21:37:51 -0400

    solved multiple definitions bug

--- env.h
+++ env.h
@@ -8,12 +8,24 @@
 namespace Symbol_DB
 {
   struct Env;
+  struct Range_record;
 
   enum Sym_tag { PRIM_SYM_TAG, REC_SYM_TAG, ARRAY_SYM_TAG };
 
-  struct Prim_info;
-  struct Rec_info;
-  struct Array_info;
+  //---- struct Prim_info ----
+  struct Prim_info {
+    size_t size; 
+    Prim_info() : size(0) {} };
+    
+  //---- struct Rec_info ----
+  struct Rec_info {
+    Env *env; 
+    Rec_info() : env(NULL) {} };
+
+  //---- struct Array_info
+  struct Array_info {
+    Range_record *range_record;
+    Array_info() : range_record(NULL) {} };
 
   //---- struct Sym_info ----
   struct Sym_info {
@@ -38,27 +50,6 @@ namespace Symbol_DB
 
   };// end Env
 
-};
-
-#include "array_types.h"
-
-namespace Symbol_DB
-{
-  //---- struct Prim_info ----
-  struct Prim_info {
-    size_t size; 
-    Prim_info() : size(0) {} };
-    
-  //---- struct Rec_info ----
-  struct Rec_info {
-    Env *env; 
-    Rec_info() : env(NULL) {} };
-
-  //---- struct Array_info
-  struct Array_info {
-    Range_record *range_record;
-    Array_info() : range_record(NULL) {} };
-  
 };// end Symbol_DB
 
 #endif//__ENV_H__
